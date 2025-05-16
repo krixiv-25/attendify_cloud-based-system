d3a24f5d6d509c39fb6f5566f2cb7fb27ac00d16
krixiv-25 <krishivmohan2005@gmail.com>
2025-05-16 12:36:17 +0530

    initial_state

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+apply(plugin = "com.google.gms.google-services") // Apply Firebase plugin here
+
+android {
+    namespace = "com.example.attendanceapp_cloud"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.attendanceapp_cloud"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.1.1")) // Firebase BOM
+    implementation("com.google.firebase:firebase-analytics-ktx") // Firebase Analytics, add others if needed
+}
